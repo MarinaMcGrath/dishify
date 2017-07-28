e54132507f031ab9cc9b351713106d98f7738e56
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-07-28 16:02:24 -0500

    Add console log

--- public/app.js
+++ public/app.js
@@ -2,9 +2,7 @@ const dishifyApp = angular.module('dishifyApp', []);
 
 dishifyApp
     .controller('LoginController', function LoginController($scope) {
-        $scope.test = function(username, password) {
-            console.log('i fucking click');
-            $scope.name = username;
-            $scope.password = password;
+        $scope.test = function(name, pass) {
+            console.log(name, pass);
         }
 });
