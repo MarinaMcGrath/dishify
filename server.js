5f618e915df10f5baf49b97930ff129c6ee8bb8c
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-07-28 13:11:55 -0500

    Connect

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen('3000', () => {
+    console.log('Listening on 3000');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
