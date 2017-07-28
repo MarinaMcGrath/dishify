2d39a735eb84966203073cdfab65250a19e54bd5
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-07-28 13:45:04 -0500

    Dynamic serve

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+require('dotenv').config();
+app.set('port', (process.env.PORT || 3000));
 
-app.listen('3000', () => {
-    console.log('Listening on 3000');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.render('/index.html');
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
+app.listen(app.get('port'), () => {
+    console.log('Node app is running on port', app.get('port'));
+});
+
