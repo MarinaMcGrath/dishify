9ed7b5dd108acf90a4cd99ca6183510cb6bc1329
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-07-28 12:16:20 -0500

    Add dependencies

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const path =  require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen('3000', () => {
     console.log("Listening on 3000");
 })
 
 app.get('/', (req, res) => {
-    res.send("I'm alive!");
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body);
 })
